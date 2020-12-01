367c0ea1e876af08425ca9c058f4e9201c83b755
hanhngooo <hanhngo1795@gmail.com>
2020-12-01 11:08:48 +0100

    minor change

--- src/store/user/reducer.js
+++ src/store/user/reducer.js
@@ -1,11 +1,12 @@
 import { ADD_SCHOOL_SUCCESS } from "./actions";
 
 const initialState = {};
-
+// eslint-disable-next-line
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_SCHOOL_SUCCESS:
       return { ...state, ...action.payload };
+
     default:
       return state;
   }
